5d3446ac8815941586f1cd475dc70571141a0fcc
BuildTools <unconfigured@null.spigotmc.org>
2015-05-20 21:18:03 +0200

    Going from non-durable customers t durable subscriber

--- src/main/java/fr/utbm/projetlo54/app/Consumer.java
+++ src/main/java/fr/utbm/projetlo54/app/Consumer.java
@@ -5,9 +5,14 @@
  */
 package fr.utbm.projetlo54.app;
 
+import java.util.Properties;
 import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.transport.nio.SelectorManager.Listener;
 import org.apache.log4j.BasicConfigurator;
 
 public class Consumer {
@@ -16,27 +21,28 @@ public class Consumer {
 private static String url = ActiveMQConnection.DEFAULT_BROKER_URL;
  
 //SUbject is the name of the topic
-private static String subject = "coucou";
+private static String subject = "Coucou";
 private static String endWhile = "no";
 
-public static void main(String[] args) throws JMSException {
+public static void main(String[] args) throws NamingException,JMSException {
     
-    
+        
         BasicConfigurator.configure();
 
         // Creatng the connection
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
         Connection connection = connectionFactory.createConnection();
+        //Set the ID of the subscriber
+        connection.setClientID("1");
         connection.start();
-
         // Creating session for seding messages
         Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
-
-        // Getting the queue
-
-        Destination destination = session.createQueue(subject);
+        // Creating the topic if not existing
+        Topic topic = session.createTopic("Coucou"); 
+        //Creation of a durable subscriber
+        TopicSubscriber consumer = session.createDurableSubscriber(topic,"AppliJava");  
         // Waiting for the message
-        MessageConsumer consumer = session.createConsumer(destination);
+
         endWhile="no";
         while(endWhile.equals("no")){
         Message message = consumer.receive();
