6e78a4fb7f2fa148c6fbfebcfafa93b5f2922e18
java <java@dev>
2015-06-16 13:50:57 +0200

    Added test (all commented except coursesession)

--- src/main/java/fr/utbm/projetlo54/app/App.java
+++ src/main/java/fr/utbm/projetlo54/app/App.java
@@ -15,6 +15,7 @@ import fr.utbm.projetlo54.repository.HibernateCourseDAO;
 import fr.utbm.projetlo54.repository.HibernateCourseSessionDAO;
 import fr.utbm.projetlo54.service.LocationService;
 import fr.utbm.projetlo54.entity.Course;
+import fr.utbm.projetlo54.repository.HibernateLocationDAO;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -28,7 +29,7 @@ import javax.naming.NamingException;
  */
 public class App {
     
-    public static void main(String[] arg)throws JMSException, NamingException{
+    public static void main(String[] arg)throws JMSException, NamingException, ParseException{
         /*JdbcformationDAO jdbc = new JdbcformationDAO();
         jdbc.connect();
         //new Producer(topic);
@@ -49,16 +50,47 @@ public class App {
         catch(ParseException e){
             System.out.println(e);
         }
-        String keyword = "Processus";
-        Location l = new Location();
-        l.setCity("Belfort");
-        l.setId(1);        
-        List<Object[]> courses = hiber.findCoursesByCritariaWithNextCourseSessions(keyword, date, l);
+        String keyword = "Processus";*/
+        /*List<Object[]> courses = hiber.findCoursesByCritariaWithNextCourseSessions(keyword, date, l);
         for(Object[] ob : courses){
             Course course = (Course) ob[0];
             CourseSession courseSession = (CourseSession) ob[1];
             System.out.println(course.getTitle() + " : " + courseSession.getCourse() + " : " + courseSession.getStartDate());
         }*/
         
+        
+        //Test add new Location : OK
+        Location l = new Location();
+        l.setCity("Paris");
+        l.setId(20);
+        //new HibernateLocationDAO().addLocation(l);
+        
+        //Test add new Course : OK
+        Course cours = new Course();
+        cours.setCode("re51");
+        cours.setTitle("reseau");
+        //new HibernateCourseDAO().addCourse(cours);
+                
+        //Test add new Client : OK
+        Client cl = new Client();
+        cl.setFirstName("quentin");
+        cl.setLastName("barthelemy");
+        cl.setSessionID(1);
+        cl.setEmail("quentin.barthelemy@utbm.fr");
+        cl.setPhone("0606060606");
+        cl.setAddress("3 rue gaston defferre 90000 Belfort");
+        cl.setId(10);
+        //new HibernateClientDAO().addClient(cl);
+        
+        //Test add new courseSession
+        CourseSession cs = new CourseSession();
+        cs.setCourse(cours.getCode());
+        cs.setLocation(l.getId());
+        Date d = new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2001");
+        cs.setStartDate(d);
+        d= new SimpleDateFormat("dd/MM/yyyy").parse("01/01/2005");
+        cs.setEndDate(d);
+        cs.setId(102);
+        new HibernateCourseSessionDAO().addCourseSession(cs);
     }
 }
