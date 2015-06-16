5b9b35e67f9f94bd61950ca73b9cbf5de8313772
java <java@dev>
2015-06-16 19:11:38 +0200

    Re-established many-to-one and on-to-many mapping

--- src/main/java/fr/utbm/projetlo54/repository/HibernateCourseSessionDAO.java
+++ src/main/java/fr/utbm/projetlo54/repository/HibernateCourseSessionDAO.java
@@ -32,17 +32,12 @@ public class HibernateCourseSessionDAO
         int csid = -1;
         try
         {
-            if(cs==null)
-            {
-            session.flush(); 
-            session.close();
-            }
-            else {
+            
             session.beginTransaction();
             session.merge(cs);
             session.getTransaction().commit();
             csid = cs.getId();
-            }
+            
         }
         catch (HibernateException e)
         {
